44b0a8a6f1b7dd143cdf6f15ee5396846f9b715a
BiswajitPrusty <biswajitprusty165@gmail.com>
2025-01-16 20:27:37 +0530

    clean up

--- HelloWorld.java
+++
@@ -1,6 +0,0 @@
-class HelloWorld{
-
-public static void main(String args[]){
-System.out.println("Namasthe Jenkins mama!! Did some changes from GitHub GUI");
-  System.out.println("Trying webhooks");
-}}
