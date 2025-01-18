3a33bb0f9546cb56e0b7f3bffad97fd11af38fad
BiswajitPrusty <biswajitprusty165@gmail.com>
2025-01-18 22:38:18 +0530

    module setup

---
+++ services/config-server/src/test/java/com/biswa/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biswa.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
